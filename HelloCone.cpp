d60dc3c3f091bf85c85375794bec72b6ab8e2aff
Mark Millard <mark.s.millard@gmail.com>
2024-08-09 17:13:30 -0600

    Tweak copyright date.

--- HelloCone.cpp
+++ HelloCone.cpp
@@ -2,7 +2,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2019-2924 Wizzer Works
+// Copyright (c) 2019-2024 Wizzer Works
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
